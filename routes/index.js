1bf72d571a726e363eb9a983c2b8e256f8d18360
Naim Miah <mmiah@fordham.edu>
2017-03-14 10:04:16 -0400

    working with ips arent hard

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,30 @@
 var express = require('express');
+var geoip = require('geoip-lite');
 var router = express.Router();
 var cmd = require('node-cmd');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-if(req['headers']['user-agent'].includes("curl")){
-	console.log("Agent exist!")
-}
-cmd.get(
-        'wttr nyc',
-        function(data){
-            data = data.slice(0, data.search("New feature"))
-            if(req['headers']['user-agent'].includes("curl")){
-        res.send(data);
-} else {
-		res.redirect('https://naimmiah.com/');
-}       
-    console.log(data)
-        }
-    )
-  
-});
+    var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+     console.log(ip)
+var geo = geoip.lookup(ip);
+var location = geo['city'] +', '+geo['region']+', '+geo['country']
+    cmd.get(
+            'wttr '+location,
+            function(data){
+                data = data.slice(0, data.search("New feature"))
+                if(req['headers']['user-agent'].includes("curl")){
+            res.send(data);
+    } else {
+    		res.redirect('https://naimmiah.com/');
+    }       
+        console.log(data)
+            }
+        )
+      
+    });
 
 module.exports = router;
+
