c2066edf1a87ff2c2aabdeabeac66e71263448c6
Naim <mmiah@fordham.edu>
2017-11-12 22:49:23 +0000

    Changed a few things around. Primary server related nginxs, added system service, etc.

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,40 @@ router.get('/', function(req, res, next) {
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
-     console.log(ip)
-var geo = geoip.lookup(ip);
-var location = geo['city'] +', '+geo['region']+', '+geo['country']
-    cmd.get(
-            'wttr '+location,
-            function(data){
+
+     //purpose of location
+     var geo = geoip.lookup(ip);
+     var location = geo['city'] +', '+geo['region']+', '+geo['country']
+
+ cmd.get(
+        'curl wttr.in/@'+ip,
+        function(err, data, stderr){
+            if(!err) {
+            if(req['headers']['user-agent'].includes("curl")){
                 data = data.slice(0, data.search("New feature"))
-                if(req['headers']['user-agent'].includes("curl")){
-            res.send(data);
-    } else {
-    		res.redirect('https://naimmiah.com/');
-    }       
-        console.log(data)
+                res.send(data);
+            } else {
+    		  res.redirect('https://naimmiah.com/');
+            }       
+            } else {
+                console.log(err)
             }
-        )
-      
+        }
+    );
+//    cmd.get(
+//            'curl wttr.in',
+//            function(data){
+//                console.log(data);
+//                data = data.slice(0, data.search("New feature"))
+//                if(req['headers']['user-agent'].includes("curl")){
+//            res.send(data);
+//    } else {
+//    		res.redirect('https://naimmiah.com/');
+//    }       
+//        console.log(data)
+//            }
+//        )
+//      
     });
 
 module.exports = router;
